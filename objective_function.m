e3c6d6649b6094c6365bdb9d80cc8814651728ff
Steven Tartakovsky <tartakovsky.developer@gmail.com>
2017-12-22 19:05:48 -0800

    abstract away assignments

--- objective_function.m
+++ objective_function.m
@@ -1,8 +1,6 @@
 % Example function - replace this with the function you are trying to optimize
 % For example purposes we will use Franke's function: https://www.sfu.ca/~ssurjano/franke2d.html
-function value = objective_function(assignments)
-  x = assignments.x;
-  y = assignments.y;
+function value = objective_function(x, y)
   value = exp(-((9*x-2).^2 + (9*y-2).^2)/4)/2 + ...
           3*exp(-(9*x+1).^2/49 - (9*y+1)/10)/4 + ...
           exp(-((9*x-7).^2 + (9*y-3).^2)/4)/2 - ...
